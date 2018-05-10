dc6b247072c7096a4ce7d3a40e003c189be74b7a
nethzyr <czobor94@gmail.com>
2018-05-10 16:55:25 +0200

    Unsubscribe in ngOnDestroy

--- src/main/webapp/app/home/restaurant-list/restaurant-list.component.ts
+++ src/main/webapp/app/home/restaurant-list/restaurant-list.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnChanges, OnDestroy} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {Restaurant, RestaurantService} from '../../entities/restaurant';
 import {JhiAlertService, JhiParseLinks} from 'ng-jhipster';
 import {Kitchen, KitchenService} from '../../entities/kitchen';
 import {Food, FoodService} from '../../entities/food';
-import {City, CityService} from "../../entities/city";
+import {City, CityService} from '../../entities/city';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
     selector: 'jhi-restaurant-list',
@@ -13,7 +14,7 @@ import {City, CityService} from "../../entities/city";
         'restaurant-list.scss'
     ]
 })
-export class RestaurantListComponent implements OnDestroy, OnChanges {
+export class RestaurantListComponent implements OnInit, OnDestroy, OnChanges {
     @Input() currentSearch: string;
     restaurants: Restaurant[];
     kitchens: Kitchen[];
@@ -21,6 +22,7 @@ export class RestaurantListComponent implements OnDestroy, OnChanges {
     cities: City[];
     totalItems: any;
     links: any;
+    subscriptions = new Subscription();
 
     constructor(
         private restaurantService: RestaurantService,
@@ -32,45 +34,48 @@ export class RestaurantListComponent implements OnDestroy, OnChanges {
     ) {
     }
 
+    ngOnInit() {
+        this.loadFilterData();
+    }
+
     ngOnDestroy() {
+        this.subscriptions.unsubscribe();
     }
 
     ngOnChanges(changes) {
         this.loadAll();
-        this.loadFilterData();
-
     }
 
     loadFilterData() {
-        this.kitchenService.list().subscribe(
+        this.subscriptions.add(this.kitchenService.list().subscribe(
             (res: HttpResponse<Kitchen[]>) => this.kitchens = res.body,
             (res: HttpErrorResponse) => this.onError(res.message)
-        );
-        this.foodService.list().subscribe(
+        ));
+        this.subscriptions.add(this.foodService.list().subscribe(
             (res: HttpResponse<Kitchen[]>) => this.foods = res.body,
             (res: HttpErrorResponse) => this.onError(res.message)
-        );
-        this.cityService.list().subscribe(
+        ));
+        this.subscriptions.add(this.cityService.list().subscribe(
             (res: HttpResponse<Kitchen[]>) => this.cities = res.body,
             (res: HttpErrorResponse) => this.onError(res.message)
-        );
+        ));
     }
 
     loadAll() {
         if (this.currentSearch) {
-            this.restaurantService.search({
+            this.subscriptions.add(this.restaurantService.search({
                 query: this.currentSearch
             }).subscribe(
                 (res: HttpResponse<Restaurant[]>) => this.onSuccess(res.body, res.headers),
                 (res: HttpErrorResponse) => this.onError(res.message)
-            );
+            ));
             return;
         }
-        this.restaurantService.query()
+        this.subscriptions.add(this.restaurantService.query()
             .subscribe(
             (res: HttpResponse<Restaurant[]>) => this.onSuccess(res.body, res.headers),
             (res: HttpErrorResponse) => this.onError(res.message)
-        );
+            ));
     }
 
     private onSuccess(data, headers) {
