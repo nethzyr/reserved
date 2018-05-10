da83bc9a6d949408ac34da9acde3935a9214d3cd
nethzyr <czobor94@gmail.com>
2018-05-10 13:48:52 +0200

    Reverting account management changes on home component

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
 import {Account, LoginModalService, Principal} from '../shared';
+import {JhiEventManager} from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-home',
@@ -19,11 +20,24 @@ export class HomeComponent implements OnInit {
 
     constructor(
         private principal: Principal,
-        private loginModalService: LoginModalService
+        private loginModalService: LoginModalService,
+        private eventManager: JhiEventManager
     ) {
     }
 
     ngOnInit() {
+        this.principal.identity().then((account) => {
+            this.account = account;
+        });
+        this.registerAuthenticationSuccess();
+    }
+
+    registerAuthenticationSuccess() {
+        this.eventManager.subscribe('authenticationSuccess', (message) => {
+            this.principal.identity().then((account) => {
+                this.account = account;
+            });
+        });
     }
 
     login() {
